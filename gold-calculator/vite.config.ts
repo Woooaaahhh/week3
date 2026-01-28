a068bafd4efccc53eb57c76ac4a7769b82912101
Andrei Asnan <andreiasnan125@gmail.com>
2026-01-28 15:55:45 +0800

    commit

--- gold-calculator/vite.config.ts
+++ gold-calculator/vite.config.ts
@@ -2,9 +2,11 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import { resolve } from 'path'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  build: {
+    outDir: 'dist'
+  },
   resolve: {
     alias: {
       '@': resolve(__dirname, 'src'),
